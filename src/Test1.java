806cfbc844e5188dc576456c6be0ab6e0e4add2e
wby95 <1587127248@qq.com>
2018-03-10 11:56:29 +0800

    .

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,11 +9,20 @@ import java.util.regex.Pattern;
 *  将多空格换成单空格
 * */
 
-
-
-
+ class parent{
+    public static int A=1;
+    static {
+        A=2;
+    }
+}
+class Sub extends parent{
+    public static int B=A;
+}
 public class Test1 {
     public static void main(String[] args) {
+        final int[]a={1,2,3};
+        int[] another={4,5,6};
+        a[0]=2;
 
         String target="";
         String s="S  tr in    g";
@@ -22,6 +30,7 @@ public class Test1 {
         Matcher m=p.matcher(s);
         target=m.replaceAll(" ");
         System.out.println(target);
+        System.out.println(Sub.B);
 
     }
 }
